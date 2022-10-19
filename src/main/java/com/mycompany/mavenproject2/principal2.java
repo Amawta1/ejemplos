a4cc483d84d7adfeed3215f560a076d30d915db9
amawt <amawt@172.16.222.221>
2022-10-19 12:23:53 -0500

    Trabajo en clase

--- src/main/java/com/mycompany/mavenproject2/principal2.java
+++ src/main/java/com/mycompany/mavenproject2/principal2.java
@@ -36,12 +36,43 @@ public class principal2 {
             a++;
             System.out.println("a = " + a);
         }
+        var numero=0;
         //leer datos del teclado
+      do{
         var lectura = new Scanner(System.in);
         
         System.out.println("Ingresar numeros menores a 10");
-        var numero=lectura.nextInt();
+        numero=lectura.nextInt();
         System.out.println("numero = " + numero);
-        
+      } while(numero<10);   
+      
+      for ( var i = 0;  i< 10; i++){
+        System.out.println("Hola " + 1);
+      }
+      var numeroList = new int[5];
+      numeroList[0]=7;
+      numeroList[1]=9;
+      numeroList[2]=4;
+      numeroList[3]=11;
+      numeroList[4]=3;
+      
+      var numeroDecimalList = new double[5];
+      numeroDecimalList[0]=8.5;
+      numeroDecimalList[1]=4.0;
+      numeroDecimalList[2]=9.3;
+      numeroDecimalList[3]=6.4;
+      numeroDecimalList[4]=3.9;
+      
+      System.out.println(numeroList[0]);
+      System.out.println(numeroList[1]);
+      System.out.println(numeroList[2]);
+      System.out.println(numeroList[3]);
+      System.out.println(numeroList[4]);
+      
+      System.out.println(numeroDecimalList[0]);
+      System.out.println(numeroDecimalList[1]);
+      System.out.println(numeroDecimalList[2]);
+      System.out.println(numeroDecimalList[3]);
+      System.out.println(numeroDecimalList[4]);
     }           
 }
