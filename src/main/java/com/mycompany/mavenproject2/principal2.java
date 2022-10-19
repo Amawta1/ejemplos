243f20d2a3bea78b6232b354682b9a9758b6dbce
amawt <amawt@172.16.222.221>
2022-10-19 11:47:35 -0500

    Trabajo en clase

---
+++ src/main/java/com/mycompany/mavenproject2/principal2.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author amawta
+ */
+public class principal2 {
+    
+    public static void maind(String[]args){
+        
+        var a=0;
+        System.out.println("a = " + a);
+        
+        a++;
+        System.out.println("a = " + a);
+        
+        a++;
+        System.out.println("a = " + a);
+        
+        a++;
+        System.out.println("a = " + a);
+        
+        a++;
+        System.out.println("a = " + a);
+        
+        a++;
+        System.out.println("a = " + a);
+        
+        while(a<=10){
+            a++;
+            System.out.println("a = " + a);
+        }
+        //leer datos del teclado
+        var lectura = new Scanner(System.in);
+        
+        System.out.println("Ingresar un numero");
+        var numero=lectura.nextInt();
+        System.out.println("numero = " + numero);
+        
+    }           
+}
